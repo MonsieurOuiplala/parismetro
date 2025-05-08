3d8c3186d2115f94f1e9d28d7e46cba3d48e25e4
illavoluntas <174935274+illavoluntas@users.noreply.github.com>
2025-05-08 13:49:23 +0200

    add search bar for stations

--- assets/script/station.js
+++ assets/script/station.js
@@ -1,3 +1,5 @@
+import { genererLignesHTML } from "./genlineicons.js";
+
 document.addEventListener("DOMContentLoaded", function () {
 	const urlParams = new URLSearchParams(window.location.search);
 	const stationId = urlParams.get("station");
@@ -20,78 +22,6 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 
 			document.title = `${station.nom} - MobiFer`;
-
-			// Génération des icônes des lignes générales
-			function genererLignesHTML(lignes) {
-				const ordreModes = ["metro", "RER", "train", "tram"]; // Ordre défini
-				const iconesModes = {
-					"metro": "/assets/icons/symbole_metro_RVB.svg",
-					"RER": "/assets/icons/symbole_RER_RVB.svg",
-					"train": "/assets/icons/symbole_train_RVB.svg",
-					"tram": "/assets/icons/symbole_tram_RVB.svg"
-				};
-			
-				function getLienLigne(type, numero) {
-					switch (type) {
-						case "metro":
-							if (numero.endsWith("bis")) {
-								return `/lines/m0${numero.replace("bis", "")}b.html`;
-							} else if (numero === "orv") {
-								return `/lines/orv.html`;
-							} else if (numero === "cgv") {
-								return `/lines/cgv.html`;
-							} else if (numero === "fun") {
-								return `lines/fun.html`
-							} else {
-								return `/lines/m${numero.padStart(2, "0")}.html`; // Métro 1 -> m01
-							}
-						case "RER":
-							return `/lines/rer${numero}.html`; // RER A -> rerA
-						case "train":
-							return `/lines/train${numero}.html`; // Transilien L -> trainL
-						case "tram":
-							// If numero == "T3a" or "T3b", return 03a ou 03b
-							if (numero.endsWith("a") || numero.endsWith("b")) {
-								return `/lines/t0${numero.replace("T", "")}.html`;
-							}
-							else {
-								return `/lines/t${(numero.replace("T", "")).padStart(2, "0")}.html`; // Tram 6 -> t06
-							}
-						default:
-							return "#"; // Sécurité si un type inconnu apparaît
-					}
-				}
-			
-				let result = [];
-			
-				// On parcourt les types de transport dans l'ordre défini
-				ordreModes.forEach(mode => {
-					let lignesFiltrees = lignes.filter(ligne => ligne.type === mode);
-					if (lignesFiltrees.length > 0) {
-						let iconeMode = `<span class="integrated"><img src="${iconesModes[mode]}" alt="${mode}"></span>`;
-			
-						let htmlLignes = lignesFiltrees.flatMap(ligne => 
-							ligne.lines.map(numero => {
-								if (numero === "cgv") {
-									var iconeLigne = `/assets/icons/LIG_IDFM_C00563.svg`
-								} else if (numero === "orv") {
-									var iconeLigne = `/assets/icons/LIG_IDFM_C01388.svg`
-								} else if (numero === "fun") {
-									var iconeLigne = `/assets/icons/funiculaire_montmartre_couleur_RVB.svg`
-								} else {
-									var iconeLigne = `/assets/icons/${mode}_${numero}_couleur_RVB.svg`; // Génération automatique
-								}
-								let lien = getLienLigne(mode, numero);
-								return `<span class="integrated"><a href="${lien}"><img src="${iconeLigne}" alt="${numero}"></a></span>`;
-							})
-						).join("&thinsp;");
-			
-						result.push(`${iconeMode}&thinsp;${htmlLignes}`); // Ajout du mode et des lignes
-					}
-				});
-			
-				return result.join(" ");
-			}
 
 			let lignesHTML = genererLignesHTML(station.lignes);
 
@@ -190,7 +120,7 @@ document.addEventListener("DOMContentLoaded", function () {
 				});
 				html += `</div>
 					<div class="license" style="margin-top: 2px !important; margin-bottom: 8px;">
-						Reproduction du visuel officiel, proposée par <a href="https://enrail.org">enrail.org</a>
+						Reproduction du visuel officiel, proposée par <a href="https://enrail.org">enrail.org</a> (Métro, RER et Transilien) et MobiFer (Tramway)
 					</div>`;
 			}
 
