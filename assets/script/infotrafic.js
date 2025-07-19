7448bfcf8844ab17cbf9b717e260aaaf78c840ae
illavoluntas <174935274+illavoluntas@users.noreply.github.com>
2025-07-19 16:23:19 +0200

    new look to pages m03b to m07

--- assets/script/infotrafic.js
+++ assets/script/infotrafic.js
@@ -9,7 +9,11 @@ fetch(`https://apitrafic.share.zrok.io/trafic/${currentLine}`, {
 .then(response => response.json())
 .then(data => {
 	const infoDiv = document.getElementById('infotrafic');
-	infoDiv.innerHTML = '';
+	if (data.rpi_last_updated) {
+		lastUpdateDate = getRelativeDateIntl(data.rpi_last_updated);
+		lastUpdateTime = new Date(data.rpi_last_updated).toLocaleTimeString('fr-FR', {hour: '2-digit', minute: '2-digit'});
+	}
+	infoDiv.innerHTML = `<p style="text-align: right; margin-top: 5px;"><em>Dernière mise à jour : ${lastUpdateDate} à ${lastUpdateTime}</em></p>`;
 	const results = data.disruptions;
 	let resultCount = 0;
 	if (results && results.length > 0) {
@@ -26,31 +30,32 @@ fetch(`https://apitrafic.share.zrok.io/trafic/${currentLine}`, {
 			if (Array.isArray(element.tags) && element.tags.includes('Actualité')) {
 				resultCount++;
 				let messages = element.messages;
-				let titre = '';
-				let contenu = '';
+				let titre = "";
+				let contenu = "";
 				messages.forEach(message => {
 					if (message.channel.name === "titre") {
-						titre = message.text;
+						titre = message.text.replace(/'/g, "’");
 					}
 					if (message.channel.name === "moteur") {
-						contenu = message.text;	
+						contenu = message.text.replace(/'/g, "’");
 					}
 				});
+				console.log(contenu)
 				if (titre && contenu) {
 					if (element.cause === "travaux") {
 						if (element.status === "active") {
-							infoDiv.innerHTML += `<h3><span class="integrated perturbation"><img src="/assets/icons/Perturbation_travaux_couleur_RVB.svg" alt="Travaux"></span> ${titre}</h3><br>${contenu}`;
+							infoDiv.innerHTML += `<h3 style='margin-bottom: 0;'><span class="integrated perturbation"><img src="/assets/icons/Perturbation_travaux_couleur_RVB.svg" alt="Travaux"></span> ${titre}</h3><br>${contenu}`;
 						}
 						else {
-							infoDiv.innerHTML += `<h3><span class="integrated"><img src="/assets/icons/Perturbation_travaux_NB_RVB.svg" alt="Travaux"></span> ${titre}</h3><br>${contenu}`;
+							infoDiv.innerHTML += `<h3 style='margin-bottom: 0;'><span class="integrated"><img src="/assets/icons/Perturbation_travaux_NB_RVB.svg" alt="Travaux"></span> ${titre}</h3><br>${contenu}`;
 						}
 					}
 					else if (element.cause === "perturbation") {
 						if (element.status === "active") {
-							infoDiv.innerHTML += `<h3><span class="integrated perturbation"><img src="/assets/icons/Perturbation_trafic_couleur_RVB.svg" alt="Perturbation"></span> ${titre}</h3><br>${contenu}`;
+							infoDiv.innerHTML += `<h3 style='margin-bottom: 0;'><span class="integrated perturbation"><img src="/assets/icons/Perturbation_trafic_couleur_RVB.svg" alt="Perturbation"></span> ${titre}</h3><br>${contenu}`;
 						}
 						else {
-							infoDiv.innerHTML += `<h3><span class="integrated"><img src="/assets/icons/Perturbation_trafic_NB_RVB.svg" alt="Perturbation"></span> ${titre}</h3><br>${contenu}`;
+							infoDiv.innerHTML += `<h3 style='margin-bottom: 0;'><span class="integrated"><img src="/assets/icons/Perturbation_trafic_NB_RVB.svg" alt="Perturbation"></span> ${titre}</h3><br>${contenu}`;
 						}
 					}
 					if (!(index === newResults.length - 1)) {
@@ -61,6 +66,17 @@ fetch(`https://apitrafic.share.zrok.io/trafic/${currentLine}`, {
 		});
 	}
 	if (resultCount === 0) {
-		infoDiv.innerHTML += '<p><span class="integrated"><img src="/assets/icons/Trafic_normal_RVB.svg" alt="Trafic normal"></span> Aucune perturbation sur votre ligne !</p>';
+		infoDiv.innerHTML += '<p><span class="integrated"><img src="/assets/icons/Trafic_normal_RVB.svg" alt="Trafic normal"></span> Aucune perturbation sur la ligne !</p>';
 	}
 })
+
+function getRelativeDateIntl(isoString) {
+	const rtf = new Intl.RelativeTimeFormat('fr', { numeric: 'auto' });
+	const date = new Date(isoString);
+	const now = new Date();
+
+	const diffInMs = now - date;
+	const diffInDays = Math.round(diffInMs / (1000 * 60 * 60 * 24));
+
+	return rtf.format(-diffInDays, 'day');
+}
